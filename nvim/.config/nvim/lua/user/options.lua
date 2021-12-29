7d33128add35ec01fe60b918c2392746e4ce62cf
lytharn <lytharn@users.noreply.github.com>
2021-12-29 19:04:46 +0100

    Make nvim config lua only

---
+++ nvim/.config/nvim/lua/user/options.lua
@@ -0,0 +1,9 @@
+vim.opt.ignorecase = true                -- Ignore case in search patterns
+vim.opt.smartcase = true                 -- Ignore case in search patterns if no upper case
+vim.opt.number = true                    -- Show line numbers
+vim.opt.relativenumber = true            -- Use relative line numbers
+vim.opt.hlsearch = true                  -- Highlight search matches
+vim.opt.mouse = "a"                      -- Allow mouse usage
+vim.opt.splitbelow = true                -- Horizontal splits go below current window
+vim.opt.splitright = true                -- Vertical splits go to the right of current window
+vim.opt.cursorline = true                -- Highlight current line
--- nvim/README.md
+++ nvim/README.md
@@ -4,17 +4,3 @@
 └── .config
     └── nvim
         └── init.vim
-
-### Install notes
-
-1.  Download plug-in manager
-    `curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim`
-1.  Install plugins
-    `nvim +PlugInstall`
-
-#### Extra notes for VS Code
-
-Make sure to have the Neo Vim plugin installed
-
-Install plugins in VS Code, by typing `:PluginInstall`
