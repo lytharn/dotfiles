86196469fe65e4f2af9ae5bf9b7217c9517a6c64
lytharn <lytharn@users.noreply.github.com>
2022-01-04 22:11:19 +0100

    Disable nvim syntax for highlighting

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -6,11 +6,11 @@ end
 configs.setup {
   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "" }, -- List of parsers to ignore installing
+  ignore_install = { "" }, -- list of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
+    additional_vim_regex_highlighting = false, -- true or list will run vim syntax also
   },
   indent = { enable = true, disable = {} },
 }
