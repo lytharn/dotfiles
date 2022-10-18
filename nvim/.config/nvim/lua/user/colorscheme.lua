a4f54cd43a08e1567a0356e20265c79a6f7a6114
lytharn <lytharn@users.noreply.github.com>
2022-10-18 20:28:31 +0200

    Use onedarker colorscheme for nvim

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,7 +1,7 @@
 -- local colorscheme = "onenord"
 -- local colorscheme = "darkplus"
--- local colorscheme = "onedarker"
-local colorscheme = "tokyonight"
+local colorscheme = "onedarker"
+-- local colorscheme = "tokyonight"
 local status_ok, colorscheme_module = pcall(require, colorscheme)
 if not status_ok then
   vim.notify("Could not load colorscheme " .. colorscheme)
