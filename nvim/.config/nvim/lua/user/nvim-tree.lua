b6c44113429dc4c457a9097d7b2f9e50d5d389ed
lytharn <lytharn@users.noreply.github.com>
2022-11-02 05:32:08 +0100

    nvim: Use 'h' and 'l' in nvim-tree

--- nvim/.config/nvim/lua/user/nvim-tree.lua
+++ nvim/.config/nvim/lua/user/nvim-tree.lua
@@ -3,4 +3,13 @@ if not status_ok then
   return
 end
 
-tree.setup{}
+tree.setup {
+  view = {
+    mappings = {
+      list = {
+        { key = "l", action = "edit" },
+        { key = "h", action = "close_node" },
+      },
+    },
+  },
+}
