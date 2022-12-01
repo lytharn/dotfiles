ad1b5c437a539d5da4839b762bd28e3efff2da63
lytharn <lytharn@users.noreply.github.com>
2022-12-01 19:17:13 +0100

    nvim: Use default options for keymaps

--- nvim/.config/nvim/lua/user/keymap.lua
+++ nvim/.config/nvim/lua/user/keymap.lua
@@ -1,53 +1,51 @@
-local opts = { noremap = true, silent = true }
-
 --Remap space as leader key
-vim.keymap.set("", "<Space>", "<Nop>", opts)
+vim.keymap.set("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
-vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
-vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
-vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 -- Navigate tabs
-vim.keymap.set("n", "<A-h>", "gt", opts)
-vim.keymap.set("n", "<A-l>", "gT", opts)
+vim.keymap.set("n", "<A-h>", "gt")
+vim.keymap.set("n", "<A-l>", "gT")
 
 -- Telescope
 local telescope = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", telescope.find_files, opts)
-vim.keymap.set("n", "<leader>fg", telescope.live_grep, opts)
-vim.keymap.set("n", "<leader>fb", telescope.buffers, opts)
-vim.keymap.set("n", "<leader>fd", telescope.diagnostics, opts)
-vim.keymap.set("n", "<leader>fo", telescope.lsp_document_symbols, opts)
-vim.keymap.set("n", "<leader>ft", telescope.lsp_workspace_symbols, opts)
-vim.keymap.set("n", "<leader>fr", telescope.lsp_references, opts)
+vim.keymap.set("n", "<leader>ff", telescope.find_files)
+vim.keymap.set("n", "<leader>fg", telescope.live_grep)
+vim.keymap.set("n", "<leader>fb", telescope.buffers)
+vim.keymap.set("n", "<leader>fd", telescope.diagnostics)
+vim.keymap.set("n", "<leader>fo", telescope.lsp_document_symbols)
+vim.keymap.set("n", "<leader>ft", telescope.lsp_workspace_symbols)
+vim.keymap.set("n", "<leader>fr", telescope.lsp_references)
 
 -- nvim-tree
 local nvim_tree = require("nvim-tree.api")
 -- toggle nvim-tree with current file selected
-vim.keymap.set("n", "<leader>e", function() nvim_tree.tree.toggle(true) end, opts)
+vim.keymap.set("n", "<leader>e", function() nvim_tree.tree.toggle(true) end)
 
 -- Insert --
 -- Press jk fast to enter
-vim.keymap.set("i", "jk", "<ESC>", opts)
+vim.keymap.set("i", "jk", "<ESC>")
 
 -- Visual --
 -- Stay in indent mode
-vim.keymap.set("v", "<", "<gv", opts)
-vim.keymap.set("v", ">", ">gv", opts)
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
 
 -- Move text up and down
-vim.keymap.set("v", "<A-j>", ":m .+1<CR>==", opts)
-vim.keymap.set("v", "<A-k>", ":m .-2<CR>==", opts)
+vim.keymap.set("v", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("v", "<A-k>", ":m .-2<CR>==")
 
 -- Do not copy what is replaced with paste
-vim.keymap.set("v", "p", '"_dP', opts)
+vim.keymap.set("v", "p", '"_dP')
 
 -- Visual Block --
 -- Move text up and down
-vim.keymap.set("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+vim.keymap.set("x", "<A-j>", ":move '>+1<CR>gv-gv")
+vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv")
