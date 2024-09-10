54685fe383a273ba0cadd10e7b3abcbe5a763c1b
lytharn <lytharn@users.noreply.github.com>
2024-09-10 20:18:28 +0200

    nvim: Add visual mode code action

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -22,8 +22,9 @@ return { -- LSP Configuration & Plugins
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("lytharn-lsp-attach", { clear = true }),
       callback = function(event)
-        local map = function(keys, func, desc)
-          vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+        local map = function(keys, func, desc, mode)
+          mode = mode or "n"
+          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
         end
 
         map("gd", require("telescope.builtin").lsp_definitions, "[G]o to [D]efinition")
@@ -35,6 +36,7 @@ return { -- LSP Configuration & Plugins
         map("<leader>fS", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[F]ind [S]ymbols in workspace")
         map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
         map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+        map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", "v")
         map("K", vim.lsp.buf.hover, "Hover Documentation")
 
         -- Highlight references of the word under the cursor.
