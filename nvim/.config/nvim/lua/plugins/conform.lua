6547f85da40e05214995f47e219ea2f4872157d7
lytharn <lytharn@users.noreply.github.com>
2025-05-30 11:23:59 +0200

    nvim: Clean up conform configuration

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -12,14 +12,8 @@ return {
       desc = "[C]ode [F]ormat",
     },
   },
-  -- Everything in opts will be passed to setup()
   opts = {
     -- Set up format-on-save
     format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
   },
-  init = function()
-    -- If you want the formatexpr, here is the place to set it
-    -- This will use the vim formatting keys gq
-    -- vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
 }
