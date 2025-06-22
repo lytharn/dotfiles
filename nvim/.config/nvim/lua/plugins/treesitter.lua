7eaf022cc052b895e9bd7980967e647a13dc79a4
lytharn <lytharn@users.noreply.github.com>
2025-06-22 07:27:09 +0200

    nvim: Add key maps for text objects

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,106 @@ return {
   "nvim-treesitter/nvim-treesitter",
   version = "9",
   build = ":TSUpdate",
-  event = "VeryLazy",
+  event = { "VeryLazy" },
+  dependencies = {
+    {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      config = function()
+        ---@diagnostic disable-next-line: missing-fields
+        require("nvim-treesitter.configs").setup({
+          textobjects = {
+            select = {
+              enable = true,
+
+              -- Automatically jump forward to textobj
+              lookahead = true,
+
+              keymaps = {
+                -- Capture groups are defined in textobjects.scm
+                ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+                ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+                ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+                ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+
+                ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
+                ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
+
+                ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+                ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+
+                ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+                ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+
+                ["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
+                ["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
+
+                ["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
+                ["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
+
+                ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+                ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+              },
+            },
+            swap = {
+              enable = true,
+              swap_next = {
+                ["<leader>na"] = "@parameter.inner", -- Swap parameters/argument with next
+                ["<leader>nm"] = "@function.outer",  -- Swap function with next
+              },
+              swap_previous = {
+                ["<leader>pa"] = "@parameter.inner", -- Swap parameters/argument with prev
+                ["<leader>pm"] = "@function.outer",  -- Swap function with previous
+              },
+            },
+            move = {
+              enable = true,
+              set_jumps = true, -- Set jumps in the jumplist, can use C-o/C-i
+              goto_next_start = {
+                ["]f"] = { query = "@call.outer", desc = "Next function call start" },
+                ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
+                ["]c"] = { query = "@class.outer", desc = "Next class start" },
+                ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
+                ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
+              },
+              goto_next_end = {
+                ["]F"] = { query = "@call.outer", desc = "Next function call end" },
+                ["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
+                ["]C"] = { query = "@class.outer", desc = "Next class end" },
+                ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
+                ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
+              },
+              goto_previous_start = {
+                ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
+                ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
+                ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+                ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
+                ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
+              },
+              goto_previous_end = {
+                ["[F"] = { query = "@call.outer", desc = "Prev function call end" },
+                ["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
+                ["[C"] = { query = "@class.outer", desc = "Prev class end" },
+                ["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
+                ["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
+              },
+            },
+          },
+        })
+
+        local repeatable_move = require("nvim-treesitter.textobjects.repeatable_move")
+
+        -- Repeat movements with ; and ,
+        vim.keymap.set({ "n", "x", "o" }, ";", repeatable_move.repeat_last_move)
+        vim.keymap.set({ "n", "x", "o" }, ",", repeatable_move.repeat_last_move_opposite)
+
+        -- Maintain builtin f, F, t, T also repeatable with ; and ,
+        vim.keymap.set({ "n", "x", "o" }, "f", repeatable_move.builtin_f_expr, { expr = true })
+        vim.keymap.set({ "n", "x", "o" }, "F", repeatable_move.builtin_F_expr, { expr = true })
+        vim.keymap.set({ "n", "x", "o" }, "t", repeatable_move.builtin_t_expr, { expr = true })
+        vim.keymap.set({ "n", "x", "o" }, "T", repeatable_move.builtin_T_expr, { expr = true })
+      end,
+    },
+  },
   init = function(plugin)
     -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
     -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
